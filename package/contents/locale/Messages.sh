3f743a7a6adcf4e6fe258e24f89b8ad4f19a56ca
wwjjbb <dxt.wjb@gmail.com>
2017-06-29 19:01:43 +0100

    Initial localisation changes

--- package/contents/locale/Messages.sh
+++ package/contents/locale/Messages.sh
@@ -1,67 +1,77 @@
-#! /usr/bin/env bash
-# Copied this script from gmail-plasmoid-0.7.20 which is copyrighted as follows:
-# Copyright (C) 2009 Mark McCans <mjmccans@gmail.com>
-# under the GPLv2 or later
-# Modifications for QML plasmoids by:
-# Copyright (C) 2012 Glad Deschrijver <glad.deschrijver@gmail.com>
+#!/bin/sh
 
-# Go to script dir
-cd "${0%/*}"
-DIR=$(pwd)
+# based on exaple found here:
+#   https://techbase.kde.org/Development/Tutorials/Localization/i18n_Build_Systems
 
-#NAME=$(basename $(dirname $DIR))
-NAME="luna-qml"
-API=$(grep "X-Plasma-API" ../../metadata.desktop | sed 's/.*=//')
-case "$API" in
-  "python") SCRIPTEXT="py" ;;
-  "ruby-script") SCRIPTEXT="rb" ;;
-  "webkit") SCRIPTEXT="js" ;;
-  "declarativeappletscript") SCRIPTEXT="js" ;;
-  * ) exit ;;
-esac
+BUGADDR="https://github.com/wwjjbb/Luna-II/issues"	# MSGID-Bugs
 
-VERSION=$(grep "X-KDE-PluginInfo-Version" ../../metadata.desktop | sed 's/.*=//')
+BASEDIR=".."	# root of translatable sources
+METADATA="../../metadata.desktop"
+WDIR=`pwd`		# working dir
 
-XGETTEXT="xgettext --from-code=UTF-8 -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 \
-          -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale \
-          -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3xgettext -ki18n -ki18nc
-          -ki18ncp -ki18np"
-EXTRACTRC="./extractrc"
+PROJECT=plasma_applet_$(grep "X-KDE-PluginInfo-Name" $METADATA | sed 's/.*=//')
+VERSION=$(grep "X-KDE-PluginInfo-Version" $METADATA | sed 's/.*=//')
 
-if [ "x$1" != "x" ]; then
-    if [ ! -d "$1" ]; then
-        mkdir -p "$1/LC_MESSAGES"
-    fi
-fi
+RCSCRIPT="$WDIR/rc.js"
 
-$EXTRACTRC ../ui/*.ui ../config/*.xml > ./rc.$SCRIPTEXT
-echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ./rc.$SCRIPTEXT
-echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ./rc.$SCRIPTEXT
-#$XGETTEXT rc.$SCRIPTEXT ../code/*.$SCRIPTEXT -o "$NAME.pot"
-$XGETTEXT ../ui/*.qml rc.$SCRIPTEXT ../code/*.$SCRIPTEXT -L Java -o "$NAME.pot"
-sed -e 's/charset=CHARSET/charset=UTF-8/g' -i "$NAME.pot"
-sed -e 's/SOME DESCRIPTIVE TITLE./plasma-applet-luna-qml language translation file./g' -i "$NAME.pot"
-sed -e "s/Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER/Copyright (C) 2011, 2012 Glad Deschrijver/g" -i "$NAME.pot"
-sed -e 's/This file is distributed under the same license as the PACKAGE package./This file is distributed under the same license as the plasma-applet-luna-qml package./g' -i "$NAME.pot"
-sed -e 's/FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.//g' -i "$NAME.pot"
-sed -e "s/Project-Id-Version: PACKAGE VERSION/Project-Id-Version: $VERSION/g" -i "$NAME.pot"
+echo "Preparing rc files"
 
-for d in */; do
-    if [ -d "$d" ]; then
-        if [ -f "$d/LC_MESSAGES/$NAME.po" ]; then
-            echo "Merging $NAME.pot -> $d/LC_MESSAGES/$NAME.po ..."
-            msgmerge -U "$d/LC_MESSAGES/$NAME.po" "$NAME.pot"
-        else
-            echo "Copying $NAME.pot -> $d/LC_MESSAGES/$NAME.po ..."
-            cp "$NAME.pot" "$d/LC_MESSAGES/$NAME.po"
-        fi
-    fi
+cd ${BASEDIR}
+# we use simple sorting to make sure the lines do not jump around too much from system to system
+#find . -name '*.qml' -o -name '*.js' | sort > ${WDIR}/rcfiles.list
+#xargs --arg-file=${WDIR}/rcfiles.list extractrc > $RCSCRIPT
+
+# additional string for KAboutData
+echo 'i18nc("NAME OF TRANSLATORS","Your names");' > $RCSCRIPT
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> $RCSCRIPT
+
+cd ${WDIR}
+
+echo "Done preparing rc files"
+
+echo "Extracting messages"
+
+cd ${BASEDIR}
+
+# see above on sorting
+find . -name '*.qml' -o -name '*.js' | sort > ${WDIR}/infiles.list
+
+cd ${WDIR}
+xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
+	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+	--msgid-bugs-address="${BUGADDR}" \
+	--files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
+
+echo "Done extracting messages"
+
+
+echo "Merging translations"
+
+catalogs=$(find . -name '*.po')
+for cat in $catalogs; do
+  echo $cat
+  msgmerge -o $cat.new $cat ${PROJECT}.pot
+  mv $cat.new $cat
 done
 
-for d in */; do
-    echo "Making $d/LC_MESSAGES/$NAME.mo ..."
-    msgfmt "$d/LC_MESSAGES/$NAME.po" -o "$d/LC_MESSAGES/$NAME.mo"
+echo "Done merging translations"
+
+echo "Generating mo files"
+
+catalogs=$(find . -name '*.po')
+for cat in $catalogs; do
+  echo $cat
+  msgfmt $cat -o ${cat%.*}.mo
 done
 
-rm -f rc.$SCRIPTEXT
-#rm -f $NAME.pot
+echo "Done generating mo files"
+
+
+
+echo "Cleaning up"
+cd ${WDIR}
+rm rcfiles.list
+rm infiles.list
+rm $RCSCRIPT
+
+echo "Done"
